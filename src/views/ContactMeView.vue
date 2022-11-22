1cf101e0e675bdbe7abb7671b85a3305801763bc
Yerffrey Romero <yerffreyromero@gmail.com>
2022-11-21 22:14:33 -0300

    :alien: under construction page before updates

---
+++ src/views/ContactMeView.vue
@@ -0,0 +1,18 @@
+<template lang="">
+  <div>
+    <under-construction-view />
+  </div>
+</template>
+<script lang="ts">
+import UnderConstructionView from '@/components/errors/UnderConstructionView.vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'blog-view',
+  components: {
+    UnderConstructionView
+  }
+})
+</script>
+<style lang="">
+</style>
