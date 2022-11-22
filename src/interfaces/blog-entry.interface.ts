3ce4c5f3a17eba6ba3dc10bf2ac0545a61dc7f9d
Yerffrey Romero <yerffreyromero@gmail.com>
2022-11-21 22:46:27 -0300

    :alien: :ambulance: modify router fix

--- src/interfaces/blog-entry.interface.ts
+++ src/interfaces/blog-entry.interface.ts
@@ -1,12 +1,12 @@
+export interface PostImages {
+  title?: string;
+  content?: string;
+}
+
 export interface BlogEntry {
   slug: string;
   title: string;
   resume: string;
   content: string;
   images?: PostImages
-}
-
-export interface PostImages {
-  title?: string;
-  content?: string;
 }
